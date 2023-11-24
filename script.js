1e69f7074a1f83d38cbd15737756b688c4c72ded
chadnelson101 <chadnelson279@gmail.com>
2023-11-24 09:19:14 +0200

    new commit

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function triangleArea(){
-    if(base > 0 && height > 0) {
-        let area = 0.5 * base * height;
-        return area;
-    }
-}
-let base = 5;
-let height = 8;
-let area = triangleArea(base, height);
+document.getElementById('cal').addEventListener('click', 
+function(){
+    let heightInput = parseFloat(document.getElementById('height').value)
+    let baseInput = parseFloat(document.getElementById('base').value)
 
-console.log(`Traingle Area ${area}`);
+    if(baseInput > 0 && heightInput > 0) {
+        let area = 0.5 * baseInput * heightInput;
+
+        document.getElementById('results').value = area;
+        } else{
+            alert('both base and hieght must be postive numbers');
+        }
+
+})
+let area = triangleArea(baseInput, heightInput);
